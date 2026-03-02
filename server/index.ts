4189f2bdda4014565107d75141b21a84fe924a25
Luciano Elish <lucianoelish05@gmail.com>
2026-03-02 14:40:10 -0500

    Added index.ts file to test/practice express and see how it works

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const PORT = 3000
+const SERVER = "localhost"
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+    .get("/suny", (req, res) => {
+        res.send("Hello Suny")
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http:://${SERVER}:${PORT}`)
+})
