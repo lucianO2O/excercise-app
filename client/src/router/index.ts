d20bfe732f6bc6b90db8642e2238751a237712b2
Luciano Elish <lucianoelish05@gmail.com>
2026-03-06 23:50:34 -0500

    Edited imports to include ActivityView and edited paths/names

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ActivityView from '../views/ActivityView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,12 +10,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/activity',
+      name: 'activity',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/ActivityView.vue'),
     },
   ],
 })
