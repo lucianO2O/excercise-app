78c15a36dae200ecd3935e13672755d055973433
Luciano Elish <lucianoelish05@gmail.com>
2026-03-09 16:14:49 -0400

    Added hamburger reactivity, added logo to website

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+
+const isActive = ref(false);
 </script>
 
 <template>
   <nav class="navbar has-background-link-dark" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <RouterLink class="navbar-item" to="/">
-        <img src="" alt="Logo">
+      <RouterLink class="navbar-item " to="/">
+        <img src="./images/chud-walker-logo.png" alt="Logo">
       </RouterLink>
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="isActive = !isActive"
+        :class="{ 'is-active': isActive }" data-target="navbar-menu">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
@@ -16,7 +20,7 @@ import { RouterLink } from 'vue-router'
       </a>
     </div>
 
-    <div class="navbar-menu">
+    <div class="navbar-menu" :class="{ 'is-active': isActive }">
       <div class="navbar-start">
         <RouterLink class="navbar-item has-icons-left is-active" to="/activity">
           <span class="icon is-small">
@@ -50,4 +54,11 @@ import { RouterLink } from 'vue-router'
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar-brand .navbar-item img {
+  height: 3.25rem;
+  width: auto;
+  max-width: 100%;
+  object-fit: contain;
+}
+</style>
