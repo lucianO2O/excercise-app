6c7f26b1ac01e176366905cf0dd945c2473c0065
Luciano Elish <lucianoelish05@gmail.com>
2026-03-12 18:41:22 -0400

    Fixed some syntax

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,44 +1,46 @@
 <script setup lang="ts">
+// import { ref } from 'vue'
+// import { useUsersStore } from '@/stores/users';
 
+// const users = useUsersStore()
 </script>
 
 <template>
-  <div class="columns is-centered">
-
-    <div class="column container box is-three-quarters mt-6 p-5">
-      <div>
-        <h1 class="title is-2">Log In</h1>
+  <main>
+    <div class="columns is-centered">
+      <div class="column container box is-three-quarters mt-6 p-5">
+        <div>
+          <h1 class="title is-2">Log In</h1>
+        </div>
+        <br>
+        <div class="field pb-3">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Email/Username" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field pb-3">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" type="password" placeholder="Password" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+        <div class="button is-link is-inverted">
+          Enter
+        </div>
       </div>
-      <br>
-      <div class="field pb-3">
-        <p class="control has-icons-left has-icons-right">
-          <input class="input" type="email" placeholder="Email" />
-          <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-          </span>
-          <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-          </span>
-        </p>
-      </div>
-      <div class="field pb-3">
-        <p class="control has-icons-left has-icons-right">
-          <input class="input" type="password" placeholder="Password" />
-          <span class="icon is-small is-left">
-            <i class="fas fa-lock"></i>
-          </span>
-          <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-          </span>
-        </p>
-      </div>
-
-      <div class="button is-link is-inverted">
-        Enter
-      </div>
-
     </div>
-  </div>
+  </main>
 </template>
 
 <style scoped>
