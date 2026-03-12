54b02477b56875e87910814780e3f29b1c74572e
Luciano Elish <lucianoelish05@gmail.com>
2026-03-12 18:38:55 -0400

    Added statistics page, moved added path to index.ts in router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ActivityView from '../views/ActivityView.vue'
 import LoginView from '../views/LoginView.vue'
+import StatisticsView from '@/views/StatisticsView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,6 +20,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: StatisticsView
     }
   ]
 })
---
+++ client/src/views/StatisticsView.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="columns container mt-4 p-5">
+    <div class="column container has-text-centered">
+      <h1 class="title is-3">Statistics</h1>
+    </div>
+  </div>
+
+  <div class="columns container has-text-centered">
+    <div class="column container box has-text-centered m-3">
+      <h1 class="title is-5">Today's Workout</h1>
+      <p></p>
+      <div class="columns is-multiline container pb-4">
+        <div class="column container is-half">
+          Distance
+        </div>
+        <div class="column container is-half">
+          Duration
+        </div>
+        <div class="column container is-half">
+          Average Pace
+        </div>
+        <div class="column container is-half">
+          Calories Burned
+        </div>
+      </div>
+    </div>
+
+    <div class="column container box has-text-centered m-3">
+      <h1 class="title is-5">Yesterday's Workout</h1>
+      <p></p>
+      <div class="columns is-multiline container pb-4">
+        <div class="column container is-half">
+          Distance
+        </div>
+        <div class="column container is-half">
+          Duration
+        </div>
+        <div class="column container is-half">
+          Average Pace
+        </div>
+        <div class="column container is-half">
+          Calories Burned
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="columns container has-text-centered">
+    <div class="column container box has-text-centered m-3">
+      <h1 class="title is-5">Last Week's Workout</h1>
+      <p></p>
+      <div class="columns is-multiline container pb-4">
+        <div class="column container is-half">
+          Distance
+        </div>
+        <div class="column container is-half">
+          Duration
+        </div>
+        <div class="column container is-half">
+          Average Pace
+        </div>
+        <div class="column container is-half">
+          Calories Burned
+        </div>
+      </div>
+    </div>
+
+    <div class="column container box has-text-centered m-3">
+      <h1 class="title is-5">Yesterday's Workout</h1>
+      <p></p>
+      <div class="columns is-multiline container pb-4">
+        <div class="column container is-half">
+          Distance
+        </div>
+        <div class="column container is-half">
+          Duration
+        </div>
+        <div class="column container is-half">
+          Average Pace
+        </div>
+        <div class="column container is-half">
+          Calories Burned
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
