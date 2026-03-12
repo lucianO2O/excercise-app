8079bf0c97e079794e1b5650b1f5805388d2f118
Luciano Elish <lucianoelish05@gmail.com>
2026-03-12 18:40:52 -0400

    Created modal component for adding exercises in activity view, fleshed out activity view with placeholder cards

--- client/src/views/ActivityView.vue
+++ client/src/views/ActivityView.vue
@@ -1,8 +1,91 @@
 <script setup lang="ts">
+import ExerciseModal from '@/components/ExerciseModal.vue';
+import { ref } from 'vue';
+
+const addExercise = ref(ExerciseModal);
+
 </script>
 
 <template>
   <main>
-    <h1 class="title is-4 mt-4">This is the activity page. Find your activity here</h1>
+    <div class="columns">
+      <div class="column container is-centered has-text-centered mt-4 p-5">
+        <h1 class="title is-2">Your Activity</h1>
+        <p class="subtitle is-5">Find your activity here.</p>
+
+        <hr>
+
+        <button class="button is-large" >
+          Add Exercise
+        </button>
+
+        <div class="columns container has-text-centered">
+          <div class="column container box has-text-centered m-3">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                  iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+                  <a href="#">#responsive</a>
+                  <br />
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column container box has-text-centered m-3">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/assets/images/placeholders/96x96.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                  iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+                  <a href="#">#responsive</a>
+                  <br />
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped></style>
