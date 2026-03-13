b1cb02f6dfc06f30f33a7f690136ca4737a019d7
Luciano Elish <lucianoelish05@gmail.com>
2026-03-12 21:46:31 -0400

    Added admin and user refs, added an authenticate user function

--- client/src/stores/users.ts
+++ client/src/stores/users.ts
@@ -1,10 +1,20 @@
 import data from '../data/users.json'
 import { defineStore } from 'pinia'
 import type { User } from '@/types'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useUsersStore = defineStore('users', () => {
   const users = ref(data.users)
+  const user = ref<User | null>(null)
 
-  return { users }
+  const admin = computed(() => user.value?.username === 'admin' ? user.value : null)
+
+  const authUser = (identifier: string, password: string): User | null => {
+    const found = users.value.find(
+      (user) => (user.username === identifier || user.email === identifier) && user.password === password
+    )
+    return found || null // return found user or null if not found
+  }
+
+  return { users, user, admin, authUser }
 })
