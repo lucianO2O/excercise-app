55b0e80cfd4e9655a2548b4f892128553178c43a
Luciano Elish <lucianoelish05@gmail.com>
2026-03-15 23:53:04 -0400

    Changed admin to be based on given role in user data

--- client/src/stores/users.ts
+++ client/src/stores/users.ts
@@ -7,13 +7,13 @@ export const useUsersStore = defineStore('users', () => {
   const users = ref(data.users)
   const user = ref<User | null>(null)
 
-  const admin = computed(() => user.value?.username === 'admin' ? user.value : null)
+  const admin = computed(() => user.value?.role === 'admin' ? user.value : null)
 
   const authUser = (identifier: string, password: string): User | null => {
     const found = users.value.find(
       (user) => (user.username === identifier || user.email === identifier) && user.password === password
     )
-    return found || null // return found user or null if not found
+    return found || null // return user or null if not found
   }
 
   return { users, user, admin, authUser }
