ae97bdf12614f63ae35da31c67b79dea6845a180
Luciano Elish <lucianoelish05@gmail.com>
2026-03-16 15:51:22 -0400

    Fixed how statistics are arranged, added constants to allow each statistic box to be updated with its respective statistics relative to date

--- client/src/views/StatisticsView.vue
+++ client/src/views/StatisticsView.vue
@@ -1,93 +1,97 @@
 <script setup lang="ts">
+import { useUsersStore } from '@/stores/users';
+import { useExerciseStore } from '@/stores/exerciseStore';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 
+const userStore = useUsersStore()
+const { user } = storeToRefs(userStore)
+const exerciseStore = useExerciseStore()
+
+
+const today = new Date().toISOString().split('T')[0]
+const weekStart = new Date(Date.now() - 7 * 86400000).toISOString().split('T')[0]
+
+const todayStats = computed(() => exerciseStore.getStats(user.value!.id, today, today))
+const thisWeekStats = computed(() => exerciseStore.getStats(user.value!.id, weekStart))
+const allTimeStats = computed(() => exerciseStore.getStats(user.value!.id))
 </script>
 
 <template>
-  <div class="columns container mt-4 p-5">
-    <div class="column container has-text-centered">
-      <h1 class="title is-3">Statistics</h1>
+  <div class="container mt-4 p-5" v-if="user">
+    <h1 class="title is-2 has-text-centered">Statistics</h1>
+    <p class="subtitle has-text-centered">Welcome, {{ user.firstName }}</p>
+    <hr>
+
+    <div class="columns">
+      <div class="column box has-text-centered m-3">
+        <h1 class="title is-5">Today</h1>
+        <div class="columns is-multiline">
+          <div class="column is-half">
+            <p class="heading">Distance</p>
+            <p class="title is-4">{{ todayStats.distance }} mi</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Duration</p>
+            <p class="title is-4">{{ todayStats.duration }} min</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Avg Pace</p>
+            <p class="title is-4">{{ todayStats.pace.toFixed(1) }} m/hr</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Calories</p>
+            <p class="title is-4">{{ todayStats.calories }}</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column box has-text-centered m-3">
+        <h1 class="title is-5">Last 7 Days</h1>
+        <div class="columns is-multiline">
+          <div class="column is-half">
+            <p class="heading">Distance</p>
+            <p class="title is-4">{{ thisWeekStats.distance }} mi</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Duration</p>
+            <p class="title is-4">{{ thisWeekStats.duration }} min</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Avg Pace</p>
+            <p class="title is-4">{{ thisWeekStats.pace.toFixed(1) }} m/hr</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Calories</p>
+            <p class="title is-4">{{ thisWeekStats.calories }}</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column box has-text-centered m-3">
+        <h1 class="title is-5">All Time</h1>
+        <div class="columns is-multiline">
+          <div class="column is-half">
+            <p class="heading">Distance</p>
+            <p class="title is-4">{{ allTimeStats.distance }} mi</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Duration</p>
+            <p class="title is-4">{{ allTimeStats.duration }} min</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Avg Pace</p>
+            <p class="title is-4">{{ allTimeStats.pace.toFixed(1) }} m/hr</p>
+          </div>
+          <div class="column is-half">
+            <p class="heading">Calories</p>
+            <p class="title is-4">{{ allTimeStats.calories }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-
-  <div class="columns container has-text-centered">
-    <div class="column container box has-text-centered m-3">
-      <h1 class="title is-5">Today's Workout</h1>
-      <p></p>
-      <div class="columns is-multiline container pb-4">
-        <div class="column container is-half">
-          Distance
-        </div>
-        <div class="column container is-half">
-          Duration
-        </div>
-        <div class="column container is-half">
-          Average Pace
-        </div>
-        <div class="column container is-half">
-          Calories Burned
-        </div>
-      </div>
-    </div>
-
-    <div class="column container box has-text-centered m-3">
-      <h1 class="title is-5">Yesterday's Workout</h1>
-      <p></p>
-      <div class="columns is-multiline container pb-4">
-        <div class="column container is-half">
-          Distance
-        </div>
-        <div class="column container is-half">
-          Duration
-        </div>
-        <div class="column container is-half">
-          Average Pace
-        </div>
-        <div class="column container is-half">
-          Calories Burned
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="columns container has-text-centered">
-    <div class="column container box has-text-centered m-3">
-      <h1 class="title is-5">Last Week's Workout</h1>
-      <p></p>
-      <div class="columns is-multiline container pb-4">
-        <div class="column container is-half">
-          Distance
-        </div>
-        <div class="column container is-half">
-          Duration
-        </div>
-        <div class="column container is-half">
-          Average Pace
-        </div>
-        <div class="column container is-half">
-          Calories Burned
-        </div>
-      </div>
-    </div>
-
-    <div class="column container box has-text-centered m-3">
-      <h1 class="title is-5">Yesterday's Workout</h1>
-      <p></p>
-      <div class="columns is-multiline container pb-4">
-        <div class="column container is-half">
-          Distance
-        </div>
-        <div class="column container is-half">
-          Duration
-        </div>
-        <div class="column container is-half">
-          Average Pace
-        </div>
-        <div class="column container is-half">
-          Calories Burned
-        </div>
-      </div>
-    </div>
+  <div v-else class="has-text-centered mt-6">
+    <p>Please log in to view your statistics.</p>
   </div>
 </template>
-
-<style scoped></style>
