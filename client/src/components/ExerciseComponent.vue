3f660332c4de97003b25d73974318a05661e426f
Luciano Elish <lucianoelish05@gmail.com>
2026-03-15 23:56:58 -0400

    Restructured component to only show when user is logged in, loops through exercises using user id so that each exercise created by a user is specifically for that user

--- client/src/components/ExerciseComponent.vue
+++ client/src/components/ExerciseComponent.vue
@@ -1,71 +1,80 @@
 <script setup lang="ts">
 import { useExerciseStore } from '@/stores/exerciseStore';
 import { useUsersStore } from '@/stores/users';
-import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
 
 const exercises = useExerciseStore()
 const users = useUsersStore()
+const { user } = storeToRefs(users)
 
-// const isActive = ref(false)
 </script>
 
 <template>
-  <div v-for="user in users.users" :key="user.id">
-    <article class="media box" v-if="exercises.exercise.some(ex => ex.userId === user.id)">
+  <div v-if="user">
+    <article class="media box" v-for="exercise in exercises.exercise.filter(ex => ex.userId === user?.id)"
+      :key="exercise.id">
       <figure class="media-left">
         <p class="image is-64x64">
           <img src="https://bulma.io/assets/images/placeholders/128x128.png" />
         </p>
       </figure>
-      <div class="media-content" v-for="exercise in exercises.exercise.filter(ex => ex.userId === user.id)" :key="exercise.id">
+      <div class="media-content" >
         <div class="content">
           <div>
             <p>
-              <strong class="mx-5">{{ user.firstName }} {{ user.lastName }}</strong>
-              <a><u>@{{ user.username }}</u></a>
+              <strong>{{ user.firstName }} {{ user.lastName }}</strong>
+              <a class="mx-5"><u>@{{ user.username }}</u></a>
               <small>{{ exercise.date }}</small>
             </p>
-            <br/>
 
-            <h4 class="title is-3">{{ exercise.title }}</h4>
+            <h4 class="title is-3">{{ exercise.title }} - {{ exercise.duration }} minutes</h4>
 
-            <p>{{ exercise.description }} - {{ exercise.location }}</p>
-            <br>
+            <p>{{ exercise.description }}</p>
 
             <h4 class="title is-3">{{ exercise.calories }}</h4>
-            <h6 class="subtitle is-6">Calories Burned</h6>
+            <h6 class="subtitle is-7">Calories Burned</h6>
 
             <div v-if="exercise.exerciseType === 'running' || exercise.exerciseType === 'swimming' || exercise.exerciseType === 'cycling'">
-              <br>
-              <h4 class="title is-3">{{ exercise.pace }} m/hr</h4>
-              <h6 class="subtitle is-6">Pace</h6>
-
-              <h4 class="title is-3">{{ exercise.distance }} m</h4>
-              <h6 class="subtitle is-6">Distance</h6>
+              <div class="columns">
+                <div class="column">
+                  <h4 class="title is-3">{{ exercise.pace }} m/hr</h4>
+                  <h6 class="subtitle is-7">Pace</h6>
+                </div>
+                <div class="column">
+                  <h4 class="title is-3">{{ exercise.distance }} miles</h4>
+                  <h6 class="subtitle is-7">Distance</h6>
+                </div>
+              </div>
             </div>
-            <br>
-
-            <p>{{ exercise.duration }} minutes</p>
-
           </div>
         </div>
+
         <nav class="level is-mobile">
           <div class="level-left">
             <a class="level-item">
-              <span class="icon is-small"><i class="fas fa-reply"></i></span>
+              <span class="icon is-small">
+                <i class="fas fa-reply"></i>
+              </span>
             </a>
             <a class="level-item">
-              <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+              <span class="icon is-small">
+                <i class="fas fa-retweet"></i>
+              </span>
             </a>
             <a class="level-item">
-              <span class="icon is-small"><i class="fas fa-heart"></i></span>
+              <span class="icon is-small">
+                <i class="fas fa-heart"></i>
+              </span>
             </a>
           </div>
         </nav>
+
       </div>
+
       <div class="media-right">
-        <button class="delete"></button>
+        <button class="delete" @click="exercises.removeExercise(exercise.id)"></button>
       </div>
+
     </article>
   </div>
 </template>
