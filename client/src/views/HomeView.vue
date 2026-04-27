1b64292fb95e091c2a77dd473d5f8b9771f6df8f
Luciano Elish <lucianoelish05@gmail.com>
2026-04-27 14:06:39 -0400

    created basic landing page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,48 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { useUsersStore } from '@/stores/users'
+import { RouterLink } from 'vue-router'
 
-const router = useRouter()
-const usersStore = useUsersStore()
-
-if (usersStore.user) {
-  router.replace('/activity')
-}
 </script>
 
 <template>
   <main>
-    <h1 class="title is-4 mt-4">Welcome to the Home Page!</h1>
+    <section class="hero is-fullheight-with-navbar">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <p class="title is-1 mb-2">
+            <span class="icon mr-3"><i class="fas fa-person-running"></i></span>
+            ChudWalker
+          </p>
+          <p class="subtitle is-4 mb-6">Track your workouts. Stay consistent. Crush your goals.</p>
+
+          <div class="columns is-centered mb-6">
+            <div class="column is-2 has-text-centered">
+              <span class="icon is-large mb-2"><i class="fas fa-chart-line fa-2x"></i></span>
+              <p class="is-size-6">Track Progress</p>
+            </div>
+            <div class="column is-2 has-text-centered">
+              <span class="icon is-large mb-2"><i class="fas fa-users fa-2x"></i></span>
+              <p class="is-size-6">See Friends' Activity</p>
+            </div>
+            <div class="column is-2 has-text-centered">
+              <span class="icon is-large mb-2"><i class="fas fa-fire fa-2x"></i></span>
+              <p class="is-size-6">Log Calories & Stats</p>
+            </div>
+          </div>
+
+          <div class="buttons is-centered">
+            <RouterLink to="/register" class="button is-link is-medium">
+              Get Started
+            </RouterLink>
+            <RouterLink to="/login" class="button is-link is-outlined is-medium">
+              Log In
+            </RouterLink>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
+
+<style scoped>
+
+</style>
