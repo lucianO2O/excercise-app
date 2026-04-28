1cdab341c899b1744dc6716a37bf7aa33a0199ee
Luciano Elish <lucianoelish05@gmail.com>
2026-04-28 00:12:14 -0400

    fixed render deploy issue

--- client/src/services/myFetch.ts
+++ client/src/services/myFetch.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api/v1/'
+const API_BASE_URL = '/api/v1/'
 
 async function request<T>(url: string, init?: RequestInit): Promise<T> {
   const res = await fetch(url, {
